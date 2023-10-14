ebc80b145403bfa1b1cd4259a2589f9e0c05a15b
Sami EL KATEB <67482346+SamiElkateb@users.noreply.github.com>
2023-10-15 01:06:18 +0200

    add: 10 add hit-ball bonus

--- project.py
+++ project.py
@@ -2,7 +2,6 @@ import argparse
 import os
 import sys
 
-import cv2
 import gymnasium as gym
 import matplotlib.pyplot as plt
 import numpy as np
@@ -14,7 +13,7 @@ from tensorflow.keras.models import Model, Sequential
 from tensorflow.keras.optimizers.legacy import RMSprop
 
 CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
-CURRENT_MODEL_VERSION = "v9"
+CURRENT_MODEL_VERSION = "v10"
 MODEL_PATH = os.path.join(CURRENT_DIR, "models", CURRENT_MODEL_VERSION)
 ACTOR_PATH = os.path.join(MODEL_PATH, "actor.h5")
 CRITIC_PATH = os.path.join(MODEL_PATH, "critic.h5")
@@ -28,6 +27,7 @@ DOWN_ACTION = 3
 ACTIONS = [UP_ACTION, DOWN_ACTION]
 WIDTH = 80
 HEIGHT = 80
+SKIP_GRAPHS = [1, 2, 3, 4, 5, 7]
 
 loaded_rewards = pd.DataFrame({"reward_sum": []})
 
@@ -47,6 +47,7 @@ critic = Sequential(
         Dense(1),
     ]
 )
+print(actor.summary())
 
 actor.compile(optimizer=RMSprop(1e-4), loss="sparse_categorical_crossentropy")
 critic.compile(optimizer=RMSprop(1e-4), loss="mse")
@@ -56,8 +57,8 @@ gamma = 0.99
 
 def plot(reward_sums):
     SIZE = 273
-    data = []
-    for reward_sum in reward_sums:
+    data = {}
+    for i, reward_sum in reward_sums.items():
         arr = (
             reward_sum.rolling(window=50)
             .mean()
@@ -69,29 +70,34 @@ def plot(reward_sums):
         padded_arr = np.pad(
             arr, (0, SIZE - len(arr)), mode="constant", constant_values=np.NaN
         )
-        data.append(padded_arr)
+        data[i] = padded_arr
 
-    data = {key: value for key, value in enumerate(data)}
+    # data = {key: value for key, value in enumerate(data)}
     pd.DataFrame(data).plot()
+    plt.xlabel("Nombre d'épisode")
+    plt.ylabel("Moyenne mobile des gains / épisode")
     plt.show()
 
 
 def stats(mode):
-    existing_stats = []
+    existing_stats = {}
     non_existing_data_streak = 0
     current_data_version = 0
     while non_existing_data_streak < 2:
+        if current_data_version in SKIP_GRAPHS:
+            current_data_version += 1
+            continue
         filepath = os.path.join(
             CURRENT_DIR, "models", f"v{current_data_version}", "rewards.csv"
         )
         if os.path.exists(filepath):
-            existing_stats.append(pd.read_csv(filepath))
+            existing_stats[current_data_version] = pd.read_csv(filepath)
             non_existing_data_streak = 0
         else:
             non_existing_data_streak += 1
         current_data_version += 1
 
-    for i, stat in enumerate(existing_stats):
+    for i, stat in existing_stats.items():
         print(f"v{i} mean", stat.reward_sum.mean())
 
     if mode == "stats":
@@ -111,6 +117,22 @@ def discount_rewards(r):
     return discounted_r
 
 
+def compute_hit_ball_bonus(obs_t, obs_tp1):
+    if type(obs_t) != np.ndarray or type(obs_tp1) != np.ndarray:
+        return 0
+    crop_obs_t = ((obs_t[34:194:4, 40:142:2, 2] > 50).astype(np.uint8)).astype(float)
+    crop_obs_tp1 = ((obs_tp1[34:194:4, 40:142:2, 2] > 50).astype(np.uint8)).astype(
+        float
+    )
+    ball_t = np.argwhere(crop_obs_t[:, :-1] == 1)
+    ball_tp1 = np.argwhere(crop_obs_tp1[:, :-1] == 1)
+
+    if len(ball_t) < 1 or len(ball_tp1) < 1:
+        return 0
+    has_hit_ball = ball_t[0][1] == 49 and ball_tp1[0][1] == 47
+    return 0.1 if has_hit_ball else 0
+
+
 def crop(obs):
     if obs is None or type(obs) != np.ndarray:
         return np.zeros((HEIGHT, WIDTH))
@@ -128,10 +150,12 @@ def train_actor_critic():
 
     reward_sums = []
     for ep in range(2000):
-        Xs, ys, rewards = [], [], []
+        Xs, ys, rewards, mod_rewards = [], [], [], []
         prev_obs, obs = None, env.reset()
         for t in range(99000):
             x = np.hstack([crop(obs), crop(prev_obs)])
+
+            hit_ball_bonus = compute_hit_ball_bonus(prev_obs, obs)
             prev_obs = obs
             action_probs = actor.predict(x.reshape(-1, 80, 160, 1), verbose=0)
             ya = np.random.choice(len(ACTIONS), p=action_probs[0])
@@ -142,6 +166,8 @@ def train_actor_critic():
             Xs.append(x)
             ys.append(ya)
             rewards.append(reward)
+            mod_reward = reward + hit_ball_bonus
+            mod_rewards.append(mod_reward)
 
             # if reward != 0: print(f'Episode {ep} -- step: {t}, ya: {ya}, reward: {reward}')
 
@@ -149,7 +175,7 @@ def train_actor_critic():
                 Xs = np.array(Xs)
                 ys = np.array(ys)
                 values = critic.predict(Xs, verbose=0)[:, 0]
-                discounted_rewards = discount_rewards(rewards)
+                discounted_rewards = discount_rewards(mod_rewards)
                 advantages = discounted_rewards - values
                 # advantages = (discounted_rewards - discounted_rewards.mean()) / discounted_rewards.std()
                 print(f"adv: {np.min(advantages):.2f}, {np.max(advantages):.2f}")
@@ -160,9 +186,13 @@ def train_actor_critic():
                 reward_sum = sum(rewards)
                 reward_sums.append(reward_sum)
                 avg_reward_sum = sum(reward_sums[-50:]) / len(reward_sums[-50:])
+                mod_reward_sum = sum(mod_rewards)
 
                 print(
                     f"Episode {ep} -- reward_sum: {reward_sum}, avg_reward_sum: {avg_reward_sum}\n"
+                )
+                print(
+                    f"Episode {ep} -- mod_reward_sum: {mod_reward_sum}\n"
                 )
 
                 if ep % 10 == 0:
